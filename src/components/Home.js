1ef18e4043546745bcc55da758c27dafd42383ec
mehdi-jafarpour <m.jafarpoor04@gmail.com>
2024-11-02 10:11:57 +0400

    services done

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,7 +38,7 @@ function Home() {
       </section>
 
       {/* About Section */}
-      <section className="flex flex-col-reverse md:flex-row items-center py-40 mx-5 sm:mx-20">
+      <section className="flex flex-col-reverse md:flex-row items-center py-40 mx-5 sm:mx-20" >
           
           <div className="lg:w-1/2">
             <img
@@ -75,10 +75,47 @@ function Home() {
       </section>
 
       {/* Our Services Section */}
-      <section id="our-services">
-        {/* We'll add more details here later */}
-        <h2>Our Services</h2>
-      </section>
+      <section className="py-10 my-10 p-20 flex flex-col items-center justify-center" style={{"backgroundColor": "#F5F5F5"}}>
+      <h2 className="text-center text-2xl font-bold mb-4">{data?.services.title}</h2>
+      <p className="text-center text-gray-600 mb-8 ">{data?.services.description.slice(0, 150) + '...'}</p>
+
+      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-center md:gap-10 mt-10">
+        {/* Service 1 */}
+        <div className="flex flex-col items-center">
+          <div className="w-24 h-24 rounded-full p-5 bg-color1 border-8 border-orange-100 flex items-center justify-center text-white text-3xl font-bold" >
+            1
+          </div>
+          <h3 className="mt-4 text-lg font-semibold">Fit-Out and Joinery</h3>
+          <p className="text-center text-gray-500">Ask us for any specific design and details.</p>
+        </div>
+
+        {/* Service 2 */}
+        <div className="flex flex-col items-center">
+          <div className="w-24 h-24 rounded-full p-5 bg-color1 border-8 border-orange-100 flex items-center justify-center text-white text-3xl font-bold">
+            2
+          </div>
+          <h3 className="mt-4 text-lg font-semibold">Interior Product</h3>
+          <p className="text-center text-gray-500">We collaborate closely with designers to bring interior projects and fit-outs to life.</p>
+        </div>
+
+        {/* Service 3 */}
+        <div className="flex flex-col items-center">
+          <div className="w-24 h-24 rounded-full p-5 bg-color1 border-8 border-orange-100 flex items-center justify-center text-white text-3xl font-bold">
+            3
+          </div>
+          <h3 className="mt-4 text-lg font-semibold">Art Works</h3>
+          <p className="text-center text-gray-500">The TEROMIX team is prepared to create original interior artwork for any project.</p>
+        </div>
+      </div>
+      <a
+                href="/services" 
+                className="flex items-center justify-center mt-10 px-3 py-1 sm:px-4 sm:py-2 md:px-5 md:py-2.5 border border-black text-black rounded-md hover:bg-color2 hover:text-color1 transition max-w-40"
+                style={{ borderRadius: '2px' }}
+              >
+                Our Services
+                <IoIosArrowForward className="ml-1" />
+                </a>
+    </section>
 
       {/* Partners Slider Section */}
       <section id="partners-slider">
