4dba7dc1b9bceab20483655730b8993eda6fca80
mehdi-jafarpour <m.jafarpoor04@gmail.com>
2024-11-05 11:07:22 +0400

    services first

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,29 @@
-
+import { useData } from '../context/DataContext';
 
 function Services() {
-    
+  const data = useData();
+
+  const servicetData = data.services;
+  const logo = data.logo;
   
     return (
-      <section className="text-center p-8">
-        <h1>Services</h1>
-      </section>
+      <>
+          {/* Intro Section */}
+        <section className="relative w-full h-[350px]">
+          <div
+            className="w-full h-full bg-cover bg-center z-0"
+            style={{ backgroundImage: `url(${servicetData?.background})` }}
+          >
+            <div className="absolute w-[100px] sm:w-[200px] top-4 left-0 bg-white bg-opacity-60 p-2">
+              <img src={logo} alt="Logo" />
+            </div>
+          </div>
+
+          <div className="absolute inset-0 flex items-center justify-center z-10">
+            <h1 className="text-white text-6xl">{servicetData?.title}</h1>
+          </div>
+        </section>
+      </>
     );
   }
   
