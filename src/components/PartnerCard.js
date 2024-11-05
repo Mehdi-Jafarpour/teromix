e6674baaf0f7c6e46e96844c28ee654b7e1a7789
mehdi-jafarpour <m.jafarpoor04@gmail.com>
2024-11-05 15:25:27 +0400

    partners page done

---
+++ src/components/PartnerCard.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Masonry from 'react-masonry-css';
+import Lightbox from 'react-18-image-lightbox';
+import 'react-18-image-lightbox/style.css'; 
+import './PartnersCard.css'; 
+
+function PartnersCard({ partner, onClose }) {
+  // State to handle the lightbox
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  // Masonry breakpoint columns for responsiveness
+  const breakpointColumnsObj = {
+    default: 3, // 3 columns for large screens
+    1024: 2,    // 2 columns for medium screens
+    768: 1      // 1 column for smaller screens
+  };
+
+  const openLightbox = (index) => {
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50 overflow-y-auto">
+      <div className="bg-white p-6 relative w-full max-w-4xl max-h-[90vh] overflow-y-auto">
+        {/* Close Button */}
+        <button onClick={onClose} className="absolute top-3 right-3 text-white bg-black hover:text-color1 p-2">
+          Close
+        </button>
+
+        {/* Masonry Photo Grid */}
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="masonry-grid"
+          columnClassName="masonry-grid_column"
+        >
+          {/* Partner Information */}
+          <div className="masonry-item space-y-4 p-5 border">
+            <h2 className="text-2xl font-semibold">{partner.name}</h2>
+            <p className="text-gray-700">{partner.description}</p>
+            <p className="text-gray-700">Category: {partner.category}</p>
+          </div>
+
+          {partner.photos.map((photo, index) => (
+            <div key={index} className="masonry-item">
+              <img
+                src={photo}
+                alt={`Partner ${partner.id}`}
+                className="w-full h-full object-cover transition-transform transform hover:scale-105 cursor-pointer"
+                onClick={() => openLightbox(index)}
+                loading="lazy"
+              />
+            </div>
+          ))}
+        </Masonry>
+
+        {/* Lightbox */}
+        {isOpen && (
+          <Lightbox
+            mainSrc={partner.photos[photoIndex]}
+            nextSrc={partner.photos[(photoIndex + 1) % partner.photos.length]}
+            prevSrc={partner.photos[(photoIndex + partner.photos.length - 1) % partner.photos.length]}
+            onCloseRequest={() => setIsOpen(false)}
+            onMovePrevRequest={() =>
+              setPhotoIndex((photoIndex + partner.photos.length - 1) % partner.photos.length)
+            }
+            onMoveNextRequest={() =>
+              setPhotoIndex((photoIndex + 1) % partner.photos.length)
+            }
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default PartnersCard;
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,30 +1,72 @@
+import { useState } from 'react';
 import { useData } from '../context/DataContext';
+import PartnerCard from './PartnerCard';
+
 
 function Partners() {
   const data = useData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPartner, setSelectedPartner] = useState(null);
+
+  if (!data || !data.partners) return null;
   const partnersData = data.partners;
   const logo = data.logo;
-  console.log(partnersData)
-  
-    return (
-      <>
-         {/* Intro Section */}
-        <section className="relative w-full h-[350px]">
-          <div
-            className="w-full h-full bg-cover bg-center z-0"
-            style={{ backgroundImage: `url(${partnersData?.background})` }}
-          >
-            <div className="absolute w-[100px] sm:w-[200px] top-4 left-0 bg-white bg-opacity-60 p-2">
-              <img src={logo} alt="Logo" />
-            </div>
-          </div>
 
-          <div className="absolute inset-0 flex items-center justify-center z-10">
-            <h1 className="text-white text-6xl">{partnersData?.title}</h1>
+  const handleCardClick = (partner) => {
+    setSelectedPartner(partner);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      {/* Intro Section */}
+      <section className="relative w-full h-[350px]">
+        <div
+          className="w-full h-full bg-cover bg-center z-0"
+          style={{ backgroundImage: `url(${partnersData?.background})` }}
+        >
+          <div className="absolute w-[100px] sm:w-[200px] top-4 left-0 bg-white bg-opacity-60 p-2">
+            <img src={logo} alt="Logo" />
           </div>
-        </section>
-      </>
-    );
-  }
-  
-  export default Partners;
+        </div>
+
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <h1 className="text-white text-6xl">{partnersData?.title}</h1>
+        </div>
+      </section>
+
+      {/* Partners Section */}
+      <section className="py-20 flex flex-col items-center justify-center">
+        <h1 className="text-color1 text-3xl font-semibold my-5">{partnersData?.title}</h1>
+        <div className="w-[80%] h-full flex flex-col md:flex-row items-center justify-around flex-wrap truncate">
+          {partnersData?.cards.map((card, index) => (
+            <div
+              key={index}
+              className="w-full max-w-[250px] flex flex-col items-center my-4 mx-auto transition-transform transform hover:scale-105 cursor-pointer"
+            >
+              <div onClick={() => handleCardClick(card)} className="border rounded-sm overflow-hidden shadow-lg w-full min-h-[350px]">
+                <img
+                  src={card.photos[0]} 
+                  alt={card.name}
+                  className="w-full h-48 object-cover p-2" 
+                  loading="lazy"
+                />
+                <div className="p-4 text-center">
+                  <h3 className="text-lg font-semibold">{card.name}</h3>
+                  <p className="text-gray-500 mt-2 text-wrap">
+                    {card.description.slice(0, 50) + '...'}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      {isModalOpen && selectedPartner && (
+        <PartnerCard partner={selectedPartner} onClose={() => setIsModalOpen(false)} />
+      )}
+    </>
+  );
+}
+
+export default Partners;
